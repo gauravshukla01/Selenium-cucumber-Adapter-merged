5d0c5ac2f1125f20aa7065b573fb81c62544e5c7
Aditya.Kumar <Aditya.Kumar@QPaaSICNV1556.Fastest.com>
2019-05-13 11:53:10 +0530

    Commerica Login updates

--- src/test/java/org/cts/oneframework/cucumber/pageobjectmanager/PageObjectManager.java
+++ src/test/java/org/cts/oneframework/cucumber/pageobjectmanager/PageObjectManager.java
@@ -1,13 +1,15 @@
 package org.cts.oneframework.cucumber.pageobjectmanager;
 
 import org.cts.oneframework.cucumber.pages.HomePage;
+import org.cts.oneframework.cucumber.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 
 public class PageObjectManager{
 
 	private WebDriver driver;
 	private HomePage homePage;
-
+	private LoginPage loginPage;
+	
 	public PageObjectManager(WebDriver driver) {
 		this.driver = driver;
 	}
@@ -15,5 +17,7 @@ public class PageObjectManager{
 	public HomePage getHomePage() {
 		return (homePage == null) ? homePage = new HomePage(driver) : homePage;
 	}
-
+	public LoginPage getLoginPage() {
+		return (loginPage == null) ? loginPage = new LoginPage(driver) : loginPage;
+	}
 }
